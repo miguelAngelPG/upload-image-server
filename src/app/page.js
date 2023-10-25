b1e7fe688486a80ddcfe5489f15f5b2e9b0e653b
Miguel Pacheco <angelpachecogranillo@gmail.com>
2023-10-24 23:07:19 -0600

    handle image functions and handle form

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,50 @@
-"use client"
+"use client";
+
+import { useState } from "react"
+import Image from "next/image"
 
 function HomePage() {
+  const [file, setFile] = useState()
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!file) return
+
+    console.log(file)
+  };
+
+  const handleFileChange = (e) => {
+    if (!e.target.files?.[0]) return
+    setFile(e.target.files?.[0])
+  };
 
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="bg-zinc-950 p-5">
         <h1 className="text-4xl text-center my-4">Upload a file</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="file"
             className="bg-zinc-900 text-zinc-100 p-2 rounded block mb-2"
+            onChange={handleFileChange}
           />
 
           <button
             className="bg-green-900 text-zinc-100 p-2 rounded block w-full disabled:opacity-50"
+            disabled={!file}
           >
             Upload
           </button>
         </form>
+        {file && (
+          <Image
+            src={URL.createObjectURL(file)}
+            alt="Uploaded file"
+            className="w-64 h-64 object-contain mx-auto"
+            width={256}
+            height={256}
+          />
+        )}
       </div>
     </div>
   );
