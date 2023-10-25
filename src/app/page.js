7998a8539583b8b1b7e0e276ffbb5e39a63c224c
Miguel Pacheco <angelpachecogranillo@gmail.com>
2023-10-24 23:06:00 -0600

    form component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,27 @@
-export default function Home() {
-    return (
-        <div>Home</div>
-    )
+"use client"
+
+function HomePage() {
+
+
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="bg-zinc-950 p-5">
+        <h1 className="text-4xl text-center my-4">Upload a file</h1>
+        <form>
+          <input
+            type="file"
+            className="bg-zinc-900 text-zinc-100 p-2 rounded block mb-2"
+          />
+
+          <button
+            className="bg-green-900 text-zinc-100 p-2 rounded block w-full disabled:opacity-50"
+          >
+            Upload
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default HomePage;
